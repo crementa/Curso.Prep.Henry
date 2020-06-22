6c04396ceaaea4415ed4c2543e34036f9526df92
unknown <murciagoratattoos@gmail.com>
2020-06-22 18:50:16 -0300

    correciones 2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -59,33 +59,26 @@ function esEntero(numero) {
 }
 
 function fizzBuzz(numero) {
-  // Si "numero" es divisible entre 3, devuelve "fizz"
-  // Si "numero" es divisible entre 5, devuelve "buzz"
-  // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
-  // De lo contrario, devuelve el numero
+  if (numero % 15 === 0) return "fizbuzz";
   if (numero % 3 === 0)return "fizz";
   if (numero % 5 === 0) return "buzz";
-  if (numero % 15 === 0) return "fizbuzz";
+  
   return numero;
 }
-
 function esPrimo(numero) {
   // Devuelve "true" si "numero" es primo
   // De lo contrario devuelve "falso"
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  if (numero < 2) {
-    return false;}
-  if (numero === 2) {
-    return true;}
-  for (var i = 2; i < numero; i++) {
-    if (numero % 1 === 0) {
+  if( numero < 2) return false;
+  if(numero === 2) return true;
+  for(var i = 2; i < numero; i++) {
+    if(numero % i === 0) {
       return false;
-
     }
-    return true;
   }
+  return true;
 }
 
 
