762151e534e74a4f91ecbeebf31ad2a3d6260653
unknown <murciagoratattoos@gmail.com>
2020-06-22 16:12:04 -0300

    correciones

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -65,7 +65,7 @@ function fizzBuzz(numero) {
   // De lo contrario, devuelve el numero
   if (numero % 3 === 0)return "fizz";
   if (numero % 5 === 0) return "buzz";
-  if (numero % 3 === 0 && numero % 5 === 0) return "fizbuzz";
+  if (numero % 15 === 0) return "fizbuzz";
   return numero;
 }
 
@@ -75,8 +75,10 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  if (numero < 2) return false;
-  if (numero === 2) return true;
+  if (numero < 2) {
+    return false;}
+  if (numero === 2) {
+    return true;}
   for (var i = 2; i < numero; i++) {
     if (numero % 1 === 0) {
       return false;
